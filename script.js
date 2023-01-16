84f6dd125dd144bb1a5eb95e4d10cce98d9e61e4
ISS balloonplump <122274934+Fresor05@users.noreply.github.com>
2023-01-16 12:32:33 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+class spilleListe{
+
+    constructor(){
+
+    }
+
+    loadFromJSon(filnavn){
+
+    }
+    spillSang(index){
+
+    }
+    spillNeste(){
+
+    }
+    spillRandom(){
+
+    }
+}
+
+class sang{
+
+    constructor(artist, bildefil, lydfil, tittel, id){
+
+    }
+
+    spillSang(){
+
+    }
+}
